3f03ba9d18a67294cf12c41026623041fca7b74e
Arpit-Rajawat <Arpitsingh62650@gmail.com>
2026-03-08 22:13:48 +0530

    update alert_services

--- backend/app/services/alert_service.py
+++ backend/app/services/alert_service.py
@@ -1,11 +1,22 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.schemas.alert import CreateAlert
-from app.db import models
+from app.db.models import Alert
 
 
-def create_alert(alert_in: CreateAlert, db: Session):
-    new_alert = models.Alert(
-        user_id = alert_in.user_id,
+
+
+def create_alert(alert_in: CreateAlert, db: Session, current_user_id: int):
+
+    if not alert_in.query.strip():
+        raise HTTPException(status_code=400, detail="Query cannot be empty")
+    
+    existing_alert = db.query(Alert).filter(Alert.user_id == current_user_id, Alert.query == alert_in.query).first()
+    if existing_alert:
+        raise HTTPException(status_code=409, detail="Alert already exists")
+    
+    new_alert = Alert(
+        user_id = current_user_id,
         query = alert_in.query,
         frequency = "daily",
         source = "rss",
@@ -18,4 +29,23 @@ def create_alert(alert_in: CreateAlert, db: Session):
     db.refresh(new_alert)
     return new_alert
 
+def get_all_alerts(db: Session, current_user_id: int):
+    return db.query(Alert).filter(Alert.user_id == current_user_id).all()
+    
 
+def get_alert_by_id(db: Session, alert_id: int, current_user_id: int):
+    return db.query(Alert).filter(Alert.id == alert_id, Alert.user_id == current_user_id).first()
+    
+
+def delete_alert(db: Session, alert_id: int, current_user_id: int):
+    alert = db.query(Alert).filter(Alert.id == alert_id, Alert.user_id == current_user_id).first()   
+
+    if not alert:
+        return None
+    
+    db.delete(alert)
+    db.commit()
+    
+    return alert
+
+     
